48d64b80d81c877a6438b10e3bd79fa295018d75
Muskaan Jain <31951738+jmuskaan72@users.noreply.github.com>
2022-01-08 04:40:22 +0530

    plotting tutorial followed on dash

--- div2.py
+++ div2.py
@@ -3,15 +3,18 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from datetime import datetime as dt
+import plotly.express as px
+import pandas as pd
 
 #flask setup
 app = dash.Dash(__name__)
 server = app.server
 
+"""
 #web layout
-app.layout = html.Div([item1, item2])
+# app.layout = html.Div([item1, item2])
 
-item2 = html.Div([
+app.layout = html.Div([
     html.Div([
         #logo
         #company name
@@ -33,6 +36,29 @@ item2 = html.Div([
         #forecast-content
     ], id="forecast-content")
 ], className="stock_content")
+"""
+
+#dash layout tutorial
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
